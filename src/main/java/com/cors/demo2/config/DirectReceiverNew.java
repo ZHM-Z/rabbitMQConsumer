f4b6ecc7368bf93595cb0738ee452084c043e5d7
Zhouming <zhouming@wuxicloud.com>
2020-09-15 17:44:30 +0800

    rabbitmq 消费者---

---
+++ src/main/java/com/cors/demo2/config/DirectReceiverNew.java
@@ -0,0 +1,21 @@
+package com.cors.demo2.config;
+
+import org.springframework.amqp.rabbit.annotation.RabbitHandler;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+
+@Component
+//@RabbitListener(queues = "TestDirectQueue")//监听的队列名称 TestDirectQueue
+public class DirectReceiverNew {
+    @RabbitHandler
+    public void process(Map testMessage) {
+        System.out.println("第二个DirectReceiver消费者收到消息  : " + testMessage.toString());
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
